290decb4e3b08e33537f4eb720de85752b6c9cb3
Mingo20Percent <namntse180495@fpt.edu.vn>
2025-06-04 13:43:42 +0700

    fixx 1h45 chieu 4/6/2025

--- Fontend/src/pages/Header.jsx
+++ Fontend/src/pages/Header.jsx
@@ -13,9 +13,13 @@ const Header = () => {
         <Link to="/">
           <button className="nav-btn">Trang chủ</button>
         </Link>
-        <button className="nav-btn">Blog</button>
+        <Link to="/blog">
+          <button className="nav-btn">Blog</button>
+        </Link>
         <button className="nav-btn">Bảng xếp hạng</button>
-        <button className="nav-btn">Giới thiệu</button>
+        <Link to="/about">
+          <button className="nav-btn">Giới thiệu</button>
+        </Link>
         <button className="nav-btn">Tiến Trình Cai Thuốc</button>
       </div>
       <div className="home-auth-buttons">
