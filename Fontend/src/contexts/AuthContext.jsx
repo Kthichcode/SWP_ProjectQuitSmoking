7d45df0485dd776730fe2cf6396c3b8360290ea3
Mingo20Percent <namntse180495@fpt.edu.vn>
2025-06-12 12:51:42 +0700

    .

--- Fontend/src/contexts/AuthContext.jsx
+++ Fontend/src/contexts/AuthContext.jsx
@@ -9,14 +9,26 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
 
   useEffect(() => {
+  try {
     const storedUser = localStorage.getItem('user');
     const storedToken = localStorage.getItem('token');
 
     if (storedUser && storedToken) {
-      setUser(JSON.parse(storedUser));
-      setToken(storedToken);
+      const parsedUser = JSON.parse(storedUser);
+
+      if (parsedUser) {
+        setUser(parsedUser);
+        setToken(storedToken);
+      }
     }
-  }, []);
+  } catch (error) {
+    console.error('Lỗi khi parse localStorage:', error);
+    // Xóa dữ liệu lỗi (nếu có) để tránh lỗi vĩnh viễn
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+  }
+}, []);
+
 
   const login = (data) => {
     setUser(data.user);
