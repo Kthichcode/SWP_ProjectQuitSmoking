558b9e9c001e47713d079a14e5fb557968e2363f
TamHuynh15123 <hminhtam15123@gmail.com>
2025-06-19 18:57:19 +0700

    tâm vlog

--- Fontend/src/App.jsx
+++ Fontend/src/App.jsx
@@ -25,6 +25,8 @@ import AdminPlans from './assets/Admin/AdminPlans';
 import AdminStatistics from './assets/Admin/AdminStatistics';
 import AdminSystem from './assets/Admin/AdminSystem';
 import AdminFeedback from './assets/Admin/AdminFeedback';
+import CoachBlog from './assets/Coach/CoachBlog';
+
 function RequireRole({ role, children }) {
   const { user } = useAuth();
   const location = useLocation();
@@ -72,6 +74,7 @@ function App() {
           <Route path="users" element={<Users />} />
           <Route path="appointments" element={<Appointments />} />
           <Route path="messages" element={<Messages />} />
+          <Route path="blog" element={<CoachBlog />} />
         </Route>
         <Route
           path="/coach-payment"
